3e78aeaa065750af2775d54133ce7e1e5daad76f
Lukaz1802 <lukaz1802.ll@gmail.com>
2021-09-12 23:51:12 -0300

    Agrego Login aún sin terminar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import '../styles/Login.css'
+import logo from '../assets/logo.png'
 
 const Login = () => {
   return (
     <>
-       <div id="bgFullWidth" class="container-fluid container-full-width">
+       <div className="container-fluid ">
         <div id="overlay" class="d-flex justify-content-center align-items-center font-weight-bold">
-            <div class="container login-container">
-                <div class="row">
-                    <div class="col-md-6 mt-4 login-form-1">
+            <div className="container login-container">
+                <div className="row">
+                    <div className="col-md-6 mt-4 login-form-1">
                     </div>
-                    <div class="col-md-5 login-form-2">
+                    <div className="col-md-5 login-form-2" id="login-form-2">
                         <h3>Iniciar Sesion</h3>
+                        <div className="container container-logo col-sm-7">
+                        <img id="imagen-logo" src={logo} alt="" height="100px"  />
+                        </div>
                         <form>
                             <div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <input type="text" class="form-control" name="email" id="userEmail" placeholder="Usuario" onchange="handleChange(event)"/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <input type="password" class="form-control" name="contrasena" id="userContrasena" placeholder="Contraseña" onchange="handleChange(event)"/>
                                 </div>
-                                <div class="form-group">
+                                <div className="form-group">
                                     <button class="btn float-right btnSubmit mt-1" id="btnSubmit" type="submit">Ingresar</button>
                                 </div>
                                 
                             </div>
                             
-                            <div class="overlay">
-                                <div class="registrate">
-                              
-                                   
+                            <div className="overlay">
+                                <div class="volver">
+                                    <h1 className="text-inicio">Volver al inicio</h1>
                                 </div>
                             </div>
                         </form>
