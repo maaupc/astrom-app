55c75bed5d782edbda305420c3a96bdae69d464b
unknown <lukaz1802.ll@gmail.com>
2021-09-18 02:07:29 -0300

    agrego footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,43 +5,36 @@ import logo from '../assets/logo.png'
 const Footer = () => {
     return (
 
-<div className="main-footer">
- <div className="container">
-   <div className="row">
-      {/*Columna1 */}
-     <div className="col">
-       
-           <ul className="list-unstyled-icon">
-       <i class="fa fa-facebook fa-2x text-white mx-2" aria-hidden="true"></i>
-       <i class="fa fa-twitter fa-2x text-white mx-2" aria-hidden="true"></i>
-       <i class="fa fa-instagram fa-2x text-white mx-2" aria-hidden="true"></i>
-           </ul>
-     </div>
-     {/*Columna2 */}
-      <div className="col-logo">
-            <ul className="list-unstyled">
-        
-            <img  src={logo} width="100px" alt="logo footer"/>
-            
-      </ul>
+<>
+<footer className="main-footer">
+               
+               <div className="container">
+      <div className="row pt-5">
+        <div className="col-sm">
+        <ul className="list-unstyled-icon">
+           <i className="fa fa-facebook fa-2x text-white mx-2" aria-hidden="true"></i>
+           <i className="fa fa-twitter fa-2x text-white mx-2" aria-hidden="true"></i>
+           <i className="fa fa-instagram fa-2x text-white mx-2" aria-hidden="true"></i>
+               </ul>
+        </div>
+        <div className="col logo">
+        <img  src={logo} width="150px" alt="logo footer"/>
+        <p className="col-sm">
+                 &copy;{new Date().getFullYear()} Todos los derechos reservados.
+                 </p>
+        </div>
+        <div className="col">
+        <ul className="list-unstyled">
+                      <li>Telefono 42452364</li>
+                      <li>Tucuman,Argentina</li>
+                      <li>Av Mitre 222</li>
+              
+                        </ul>
+        </div>
       </div>
-      </div>
-      {/*Columna 3*/}
-      <div className="col">
-                    <ul className="list-unstyled">
-                  <li>Telefono 42452364</li>
-                  <li>Tucuman,Argentina</li>
-                  <li>Av Mitre 222</li>
-          
-                    </ul>
-          </div>  
-     <div className="row">
-             <p className="col-sm">
-             &copy;{new Date().getFullYear()} Todos los derechos reservados.
-             </p>
-     </div>
-   </div>
-   </div>
+    </div>
+                </footer>  
+            </>
     )
 } 
 
