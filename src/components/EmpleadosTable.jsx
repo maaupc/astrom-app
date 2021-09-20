97c619ea3f0d0b0ea125cbaa4d28a12c8294b720
Martin Mirabal <mirabalmartin@hotmail.com.ar>
2021-09-20 15:50:40 -0300

    filtro solucionado

--- src/components/EmpleadosTable.jsx
+++ src/components/EmpleadosTable.jsx
@@ -1,133 +1,108 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import  ModalEmpleado from "./modals/ModalEmpleado"
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ModalEmpleado from "./modals/ModalEmpleado";
 
 //Importo conexiones con base de datos
-import { empleadoGet, empleadoPost } from '../helpers/empleados'
-
-
-
-
+import { empleadoGet, empleadoPost } from "../helpers/empleados";
 
 const EmpleadosTable = () => {
+  const [empleados, setEmpleados] = useState({
+    response: "",
+    datos: [],
+    loading: false,
+  });
 
+  const [filtro, setFiltro] = useState([]);
 
-    const [empleados, setEmpleados] = useState({
-        response:"",
-        datos: [],
+  const [inputValue, setInputValue] = useState("");
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    empleadoGet().then((respuesta) => {
+      setEmpleados({
+        datos: respuesta.empleados,
         loading: false,
-    })
+      });
+      console.log(respuesta);
+    });
+  }, []);
 
-    const [inputValue, setInputValue] = useState('')
+  useEffect(() => {
+    const busqueda = empleados.datos.filter((empleado) => {
+      return empleado.dni.indexOf(inputValue) > -1;
+    });
+    setFiltro(busqueda);
+  }, [inputValue]);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
+  const changeInput = (e) => {
+    setInputValue(e.target.value);
+  };
 
+  const submitEmpleados = (e) => {
+    e.preventDefault();
 
-    useEffect(()=>{
-        empleadoGet().then((respuesta)=>{
-            setEmpleados({
-                datos: respuesta.empleados,
-                loading:false
-            })
-            console.log(respuesta)
-        })
-    }, [])
+    const busqueda = empleados.datos.filter((empleado) => {
+      return empleado.dni.indexOf(inputValue) > -1;
+    });
 
+    console.log(busqueda);
 
-    const changeInput = (e) => {
+    setFiltro(busqueda);
+  };
 
-        setInputValue(e.target.value);
-       
-    }
+  return (
+    <div>
+      <div>
+        <h1 className="mb-4">Empleados</h1>
+      </div>
+      <button className="btn btn-warning mb-3" onClick={handleShow}>
+        Crear empleado
+      </button>
 
-    const submitEmpleados = (e) =>{
+      <div className="input-group mx-auto">
+        <form onSubmit={submitEmpleados}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Ingrese un DNI"
+            value={inputValue}
+            onChange={changeInput}
+          />
+        </form>
+      </div>
 
-        e.preventDefault()
-
-        
-       
-        if(inputValue.length > 3){
-
-
-            setEmpleados({
-
-                ...empleados,
-                loading: true,
-    
-            })
-    
+      {/* TABLA EMPLEADOS */}
+      <table className="table table-responsive mt-3">
+        <thead className="table-dark">
+          <tr>
+            <th>DNI</th>
+            <th>EMPLEADO</th>
+            <th>PUESTO</th>
+            <th>ESTADO</th>
             
+          </tr>
+        </thead>
+        <tbody>
+          {filtro.map((empleado) => (
+            <tr key={empleado.uid}>
+              <th>{empleado.dni}</th>
+              <td>
+                {empleado.apellido}, {empleado.nombre}
+              </td>
+              <td>{empleado.puesto.nombre}</td>
+              <td>{empleado.licencia ? "LICENCIA" : "ACTIVO"}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <ModalEmpleado show={show} handleClose={handleClose} />
+    </div>
+  );
+};
 
-            //peticion
-            empleadoGet(inputValue).then(respuesta =>{
-
-                setEmpleados({
-
-                    response: respuesta.response,
-                    datos: respuesta.empleados,
-                    loading: false,
-    
-                })
-            });
-        }
-
-    }
-
-    
-    return (
-        <div>
-            <div>
-            <h1 className="mb-4">Empleados</h1>
-            </div>
-            <button className="btn btn-warning mb-3" onClick={handleShow}>Crear empleado</button>
-            
-            <div className="input-group mx-auto">
-                <form onSubmit={submitEmpleados}>
-                <input type="text" 
-                className="form-control"
-                 placeholder="Ingrese un DNI"
-                 value={inputValue}
-                 onChange={changeInput}
-                 
-                 />
-                 </form>
-            </div>
-
-
-            {/* TABLA EMPLEADOS */}
-            <table className="table table-responsive mt-3">
-                <thead className="table-dark">
-                    <tr>
-                        <th>DNI</th>
-                        <th>EMPLEADO</th>
-                        <th>PUESTO</th>
-                        <th>ESTADO</th>
-                        <th>ACCION</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {empleados.datos.map((empleado)=>(
-                        
-                        <tr key={empleado.uid}>
-                            <th>{empleado.dni}</th>
-                            <td>{empleado.apellido}, {empleado.nombre}</td>
-                            <td>{empleado.puesto.nombre}</td>
-                            <td>{empleado.licencia? "LICENCIA" : "ACTIVO"}</td>
-                            <td></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <ModalEmpleado show={show} handleClose={handleClose} />
-        </div>
-        
-        
-    )
-    
-}
-
-export default EmpleadosTable
+export default EmpleadosTable;
--- src/components/modals/ModalEmpleado.jsx
+++ src/components/modals/ModalEmpleado.jsx
@@ -3,7 +3,7 @@ import { Modal, Button } from "react-bootstrap";
 import {useState} from 'react'
 import { useEffect } from 'react'
 
-import {empleadoGet, empleadoPost, } from '../../helpers/empleados'
+import {empleadoGet, empleadoPost, empleadoPut} from '../../helpers/empleados'
 
 //import { licenciaGet } from '../../helpers/licencias'
 
@@ -57,9 +57,78 @@ export const ModalEmpleado = ({show, handleClose, actualizar}) => {
     }, [actualizar])
 
     const handleChange = ({target})=>{
+      if(target.name ==="activa"){
+          setFormValue({
+              ...formValue,
+              [target.name] : target.checked,
+          })
+
+      }else{
+          setFormValue({
+              ...formValue,
+              [target.name] : target.value
+          })
+      }
+
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    if(actualizar){
+        empleadoPut(actualizar, formValue).then((respuesta)=>{
+            if(respuesta.errors){
+                return window.alert(respuesta.errors[0].msg);
+            }
+            if(respuesta.msg){
+                window.alert(respuesta.msg)
+            }
+
+            console.log("formValue", formValue)
+
+            empleadoPut(formValue.empleado, {licencia: formValue.activa}).then((respuesta)=>{
+                if(respuesta.errors){
+                    return window.alert(respuesta.errors[0].msg);
+                }
+                if(respuesta.msg){
+                    window.alert(respuesta.msg)
+                }
+            })
+
+
+            setFormValue({
+                empleado: user.empleado.uid,
+                fecha: "",
+                motivo: "",
+                activa: false
+            })
+            handleClose()
+        })
+        
+
+
+    }else{
+        console.log("nuevo")
+        empleadoPost(formValue).then((respuesta)=>{
+            console.log(formValue)
+            console.log(respuesta)
+            if(respuesta.errors){
+                return window.alert(respuesta.errors[0].msg);
+            }
+            if(respuesta.msg){
+                window.alert(respuesta.msg)
+            }
+            setFormValue({
+                empleado: user.empleado.uid,
+                fecha: "",
+                motivo: "",
+                activa: false
+            })
+            handleClose()
+            
+        })
 
     }
-
+}
 
 
 
@@ -77,21 +146,23 @@ export const ModalEmpleado = ({show, handleClose, actualizar}) => {
                 <label>Nombre</label>
                 <input
                 type="text" 
+                maxLength="25"
                 className="form-control"
                 value={formValue.nombre} 
                 onChange={handleChange}
                 >
-                    {/* {empleados.datos.map((empleado)=>(
+                    {/* { empleados.datos.map((empleado)=>(
                         <option key={empleados.datos.uid} value={empleados.datos.dni}>
                             {empleado.apellido}, {empleado.nombre}
                         </option>
-                    ))} */}
+                    )) } */}
                 </input>
             </div>
             <div>
                 <label>Apellido</label>
                 <input type="text" 
                 className="form-control"
+                maxLength="25"
                 value={formValue.apellido} 
                 onChange={handleChange}/>
             </div>
@@ -99,6 +170,7 @@ export const ModalEmpleado = ({show, handleClose, actualizar}) => {
                 <label>Documento</label>
                 <input type="text" 
                 className="form-control"
+                maxLength="8"
                 value={formValue.dni} 
                 onChange={handleChange}/>
             </div>
@@ -107,12 +179,14 @@ export const ModalEmpleado = ({show, handleClose, actualizar}) => {
                 <input type="email" 
                 className="form-control"
                 value={formValue.email} 
+                maxLength="30"
                 onChange={handleChange}/>
             </div>
             <div>
                 <label>Puesto</label>
                 <input  id="" cols="30" rows="10"
                 className="form-control"
+                maxLength="25"
                  value={formValue.puesto}
                   onChange={handleChange}></input>
             </div>
@@ -128,7 +202,7 @@ export const ModalEmpleado = ({show, handleClose, actualizar}) => {
           <Button variant="secondary" onClick={handleClose}>
             Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSubmit}>
             Guardar cambios
           </Button>
         </Modal.Footer>        
