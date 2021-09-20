0b17f09db4ed77a6bc289a2273c1125b8b7e24e5
Norali Meriles <merilesnorali.1996@gmail.com>
2021-09-20 17:13:15 -0300

    footer N

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,36 +4,41 @@ import logo from '../assets/logo.png'
 
 const Footer = () => {
     return (
-
 <>
 <footer className="main-footer">
-               
-               <div className="container">
+      <div className="container">
       <div className="row pt-5">
-        <div className="col-sm">
+        <div className="col col-content">
+              <span>Servicios</span>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, corporis!</p>
         <ul className="list-unstyled-icon">
            <i className="fa fa-facebook fa-2x text-white mx-2" aria-hidden="true"></i>
            <i className="fa fa-twitter fa-2x text-white mx-2" aria-hidden="true"></i>
            <i className="fa fa-instagram fa-2x text-white mx-2" aria-hidden="true"></i>
-               </ul>
+           <i class="fa fa-linkedin" aria-hidden="true"></i> 
+       </ul>
         </div>
-        <div className="col logo">
-        <img  src={logo} width="150px" alt="logo footer"/>
+        <div className="col col-content">
+        <img  src={logo} width="100px" alt="logo footer" id="logo-astrom"/>
         <p className="col-sm">
                  &copy;{new Date().getFullYear()} Todos los derechos reservados.
                  </p>
+                 <p>Lorem ipsum dolor sit amet.
+                 </p>
+     
         </div>
-        <div className="col">
-        <ul className="list-unstyled">
-                      <li>Telefono 42452364</li>
-                      <li>Tucuman,Argentina</li>
-                      <li>Av Mitre 222</li>
-              
-                        </ul>
-        </div>
+      
+       <div className="col col-content">
+       <span>Contactanos</span>
+        <ul className="secundary-unstyled">
+            <li> <i class="fa fa-phone icon" aria-hidden="true"> Telefono 42452364</i></li>
+            <li><i class="fa fa-map-marker icon" aria-hidden="true"> Tucuman,Argentina</i></li>
+            <li> <i class="fa fa-thumb-tack icon" aria-hidden="true"> Av Mitre 222</i></li>
+            </ul>
+       </div> 
       </div>
     </div>
-                </footer>  
+ </footer>  
             </>
     )
 } 
--- src/routes/RouterDos.jsx
+++ src/routes/RouterDos.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 // import NavBarComponent from '../components/NavBarComponent';
+import Footer from "../components/Footer"
+import { Link } from "react-router-dom";
 import Error404 from "../pages/Error404";
 import Inicio from "../pages/Inicio";
 import Admin from "../pages/Admin";
@@ -9,7 +11,7 @@ import Admin from "../pages/Admin";
 const RouterDos = () => {
   return (
     <>
-      {/* <NavBarComponent /> */}
+      { <Footer/> }
       <Switch>
         <Route exact path="/" component={Inicio} />
         <Route exact path="/admin" component={Admin} />
