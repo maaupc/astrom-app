bf16635bd01aa175c9d75ac9cce974dd70e2c916
Mauricio Perez Cabral <maauricio@gmail.com>
2021-10-18 22:07:31 -0300

    Agregando funcionalidades en perfil y botones de paginacion

--- src/components/BtnPaginacion.jsx
+++ src/components/BtnPaginacion.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import '../styles/BtnPaginacion.css'
 
-const BtnPaginacion = ({ totPag, pagina, setPagina, limite = 10 }) => {
+const BtnPaginacion = ({ totPag, pagina, setPagina, limite = 2 }) => {
   let total = totPag;
   const nextPag = () => {
     if (total > pagina + limite) {
@@ -18,14 +19,14 @@ const BtnPaginacion = ({ totPag, pagina, setPagina, limite = 10 }) => {
   return (
     <>
       <button
-        className="btn btn-outline-success"
+        className="btn btn-outline-success btn-pgn"
         onClick={prevPag}
         disabled={pagina === 0 ? true : false}
       >
         <i className="fa fa-chevron-left"></i>
       </button>
       <button
-        className="btn btn-outline-success ms-2"
+        className="btn btn-outline-success ms-2 btn-pgn"
         disabled={total - (pagina + limite) <= 0 ? true : false}
         onClick={nextPag}
       >
